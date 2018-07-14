b7f62e2b4ed0c452f65510faa897b6816f32d4cc
Rob Flynn <rob@thingerly.com>
2018-07-14 00:35:57 -0400

    Make skeleton app

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,25 @@
+window['$$'] = (query) => {
+	return document.querySelectorAll(query);
+}
+
+window['$'] = (query) => {
+	return document.querySelector(query);
+}
+
+class TestApp {
+	constructor(query) {
+		this.component = $(query);
+	}
+
+	render() {
+		this.component.innerHTML = 'App Test';
+	}
+}
+
 const init_app = () => {
+	let app = new TestApp('#app');
+
+	app.render();
 }
 
 // Main entry point
